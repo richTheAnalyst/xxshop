5a9dd9802c213a5a2f794655c97ff2a0ba031e94
richTheAnalyst <richardkodah2003@gmail.com>
2026-05-03 21:13:34 +0000

    new change

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
-import { CartProvider } from "./CartContext";
+import { CartProvider } from "./context/CartContext";
 
 
 const geistSans = Geist({
